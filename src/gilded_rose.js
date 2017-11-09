7cd1b8fe5cc64c8132d5c542f6c2a45d7cae450e
Marek Tobias <tobi@Mareks-MacBook-Pro.local>
2017-11-09 15:28:29 +0100

    Reset to marek-tobias-1

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,25 +1,25 @@
 import {
-    lensProp,
-    over,
-    subtract,
-    inc,
-    propEq,
-    dec,
-    clamp,
-    cond,
-    always,
-    compose,
-    allPass,
-    ifElse,
-    identity,
+  lensProp,
+  over,
+  subtract,
+  inc,
+  propEq,
+  dec,
+  clamp,
+  identity,
+  cond,
+  T,
+  always,
+  compose,
+  pipe,
 } from 'ramda'
 
 export class Item {
-    constructor(name, sellIn, quality) {
-        this.name = name;
-        this.sellIn = sellIn;
-        this.quality = quality;
-    }
+  constructor(name, sellIn, quality){
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
 }
 
 
@@ -38,12 +38,11 @@ export const isCheese = nameEqual(CHEESE_NAME)
 export const isTicket = nameEqual(TICKET_NAME)
 export const isSulfuras = nameEqual(SULFURAS)
 export const isConjured = nameEqual(CONJURED)
-export const isCommonItem = (item) => !allPass[isConjured, isSulfuras, isCheese, isTicket]
 
-export const qualityLens = lensProp(QUALITY)
+export const qualityLens = lensProp(QUALITY) 
 export const overQuality = over(qualityLens)
 
-export const qualityBoundaries = clamp(0, 50)
+export const qualityBoundaries = clamp(0, 50) 
 export const increaseQuality = overQuality(inc)
 export const decreaseQuality = overQuality(dec)
 export const decreaseQualityBy2 = over(qualityLens, subtract(2))
@@ -53,66 +52,72 @@ export const sellInLens = lensProp(SELL_IN)
 export const decreaseSellIn = over(sellInLens, dec)
 
 export const increaseQualityAfterSellDate = (item) => {
-    return item.sellIn < 0 ? increaseQuality(item) : item
+  return item.sellIn < 0 ? increaseQuality(item) : item
 }
 export const decreaseQualityAfterSellDate = (item) => {
-    return item.sellIn < 0 ? decreaseQuality(item) : item
+  return item.sellIn < 0 ? decreaseQuality(item) : item
 }
 export const increaseQualityFor10DaysBeforeSellDate = (item) => {
-    return item.sellIn < 11 ? increaseQuality(item) : item
+  return item.sellIn < 11 ? increaseQuality(item) : item
 }
 export const increaseQualityFor6DaysBeforeSellDate = (item) => {
-    return item.sellIn < 6 ? increaseQuality(item) : item
+  return item.sellIn < 6 ? increaseQuality(item) : item
 }
 export const dropQualityToZeroAfterSellDate = (item) => {
-    return item.sellIn < 0 ? overQuality(always(0), item) : item
+  return item.sellIn < 0 ? overQuality(always(0), item) : item
 }
 
+// specific behavior
+export const updateTicketQuality = compose(
+  increaseQualityFor6DaysBeforeSellDate,
+  increaseQualityFor10DaysBeforeSellDate
+)
 
 // item updates
-export const updateCommonItem = compose(
-    decreaseQualityAfterSellDate,
-    decreaseQuality,
-)
 export const updateTicket = compose(
-    dropQualityToZeroAfterSellDate,
-    increaseQualityFor6DaysBeforeSellDate,
-    increaseQualityFor10DaysBeforeSellDate,
-    increaseQuality
+  decreaseSellIn,
+  boundQuality,
+  dropQualityToZeroAfterSellDate,
+  updateTicketQuality,
+  increaseQuality
 )
-export const updateCheese = compose(
-    increaseQualityAfterSellDate,
-    increaseQuality,
+export const updateCommonItem = compose(
+  decreaseSellIn,
+  boundQuality,
+  decreaseQualityAfterSellDate,
+  decreaseQuality,
 )
-export const updateConjuredItem = item => {
-   return isConjured(item) ? decreaseQualityBy2(item) : item
-}
+export const updateConjuredItem = compose(
+  decreaseSellIn,
+  boundQuality,  
+  decreaseQualityBy2
+)
+export const updateCheese = pipe(
+  increaseQuality,
+  increaseQualityAfterSellDate,
+  boundQuality,
+  decreaseSellIn
+)
+export const updateSulfuras = identity
 
-// general
+// why no pattern matching? :)
 export const updateQuality = cond([
-    [isConjured, updateConjuredItem],
-    [isCheese, updateCheese],
-    [isTicket, updateTicket],
-    [isCommonItem, updateCommonItem],
+  [isConjured, updateConjuredItem],
+  [isSulfuras, updateSulfuras],
+  [isCheese, updateCheese],
+  [isTicket, updateTicket],
+  [T, updateCommonItem],
 ])
 
-export const update = compose(
-    decreaseSellIn,
-    boundQuality,
-    updateQuality
-)
-
 export class Shop {
-    constructor(items = []) {
-        this.items = items;
+  constructor(items=[]){
+    this.items = items;
+  }
+  updateQuality() {
+    for (var i = 0; i < this.items.length; i++) {
+      this.items[i] = updateQuality(this.items[i])
     }
 
-    updateQuality() {
-        for (let i = 0; i < this.items.length; i++) {
-            const item = this.items[i]
-            this.items[i] = isSulfuras(item) ? item : update(item)
-        }
-
-        return this.items;
-    }
+    return this.items;
+  }
 }
